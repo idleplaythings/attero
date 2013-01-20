4eb0432c5b46dfe6626c14ac496f4bdddbec8b0e
Niko Nevala <niko@nevala.fi>
2013-01-20 11:56:32 +0000

    postgresify evolution script

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,38 +2,38 @@
 
 # --- !Ups
 
-CREATE TABLE `GameMap` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(256) DEFAULT '',
-  `width` tinyint(4) unsigned DEFAULT NULL,
-  `height` tinyint(3) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE game_map (
+  "id" serial,
+  "name" char(256) DEFAULT '',
+  "width" smallint DEFAULT NULL,
+  "height" smallint DEFAULT NULL,
+  PRIMARY KEY ("id")
+);
 
 
-CREATE TABLE `GameTile` (
-  `mapid` int(11) NOT NULL,
-  `tileid` int(11) DEFAULT NULL,
-  `texture` tinyint(4) DEFAULT NULL,
-  `toffset` tinyint(4) DEFAULT NULL,
-  `tmask` tinyint(4) DEFAULT NULL,
-  `elevation` tinyint(4) DEFAULT NULL,
-  `element` tinyint(4) DEFAULT NULL,
-  `eoffset` tinyint(4) DEFAULT NULL,
-  `evariance` tinyint(4) DEFAULT NULL,
-  `eangle` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`mapid`, `tileid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE game_tile (
+  "mapid" integer NOT NULL,
+  "tileid" integer DEFAULT NULL,
+  "texture" smallint DEFAULT NULL,
+  "toffset" smallint DEFAULT NULL,
+  "tmask" smallint DEFAULT NULL,
+  "elevation" smallint DEFAULT NULL,
+  "element" smallint DEFAULT NULL,
+  "eoffset" smallint DEFAULT NULL,
+  "evariance" smallint DEFAULT NULL,
+  "eangle" smallint DEFAULT NULL,
+  PRIMARY KEY ("mapid", "tileid")
+);
 
-CREATE TABLE `Game` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `leftplayer` int(11) DEFAULT NULL,
-  `rightplayer` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE game (
+  "id" serial,
+  "leftplayer" integer DEFAULT NULL,
+  "rightplayer" integer DEFAULT NULL,
+  PRIMARY KEY ("id")
+);
 
 # --- !Downs
 
-DROP TABLE `GameMap`;
-DROP TABLE `GameTile`;
-DROP TABLE `Game`;
+DROP TABLE game_map;
+DROP TABLE game_tile;
+DROP TABLE game;
