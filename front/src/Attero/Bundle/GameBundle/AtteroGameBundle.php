0f282972fbc249d83242e767cd53ad9cf1175e17
Niko Nevala <niko@nevala.fi>
2013-03-04 21:25:43 +0000

    revert back to single play app structure

--- front/src/Attero/Bundle/GameBundle/AtteroGameBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Attero\Bundle\GameBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AtteroGameBundle extends Bundle
-{
-}
--- front/src/Attero/Bundle/GameBundle/Resources/public/js/events/MoveEvent.js
+++
@@ -1,9 +0,0 @@
-var MoveEvent = function(unit, route, azimuth, turretFacing)
-{
-    this.name = "MoveEvent";
-    this.unit = unit;
-    this.start = {x:unit.position.x, y:unit.position.y};
-    this.azimuth = azimuth;
-    this.turretFacing = turretFacing;
-    this.route = route;
-}
