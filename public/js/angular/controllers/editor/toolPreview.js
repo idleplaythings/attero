005b05658a4e565cf9dcca2d6e7dc68e4fbd3296
Niko Nevala <niko@nevala.fi>
2013-04-07 23:52:22 +0100

    pass angle and offsets to preview renderer

--- public/js/angular/controllers/editor/toolPreview.js
+++ public/js/angular/controllers/editor/toolPreview.js
@@ -4,19 +4,22 @@ angular.module('attero')
     .factory('tileElementPreviewService', function() {
         var domElement = $('#tile-element-preview')[0];
         return {
-            renderPreviewForTileElement: function(tileElement) {
-                tileElement.getPreview(domElement, 0, 0, 200);
+            renderPreviewForTileElement: function(tileElement, offsetX, offsetY, angle) {
+                tileElement.getPreview(domElement, offsetX, offsetY, angle);
             }
         }
     })
     .controller('toolPreviewController', [ '$scope', 'landscapingToolService', 'tileElementPreviewService', function($scope, landscapingToolService, tileElementPreviewService) {
         $scope.landscapingToolService = landscapingToolService;
+        $scope.angle = 0;
+        $scope.offsetX = 0;
+        $scope.offsetY = 0;
 
         $scope.$watch('landscapingToolService.getTileElement()', function(tileElement) {
             if (tileElement === null) {
                 return false;
             }
 
-            tileElementPreviewService.renderPreviewForTileElement(tileElement);
+            tileElementPreviewService.renderPreviewForTileElement(tileElement, $scope.offsetX, $scope.offsetY, $scope.angle);
         });
     }]);
