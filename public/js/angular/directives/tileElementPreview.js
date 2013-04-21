4e3a0ca2cff9f9998f4020f60ef092b75a58cc44
Niko Nevala <niko@nevala.fi>
2013-04-21 22:19:32 +0100

    rudimentary rendering of tile element grid previews

--- public/js/angular/directives/tileElementPreview.js
+++ public/js/angular/directives/tileElementPreview.js
@@ -1,43 +1,29 @@
 'use strict';
 
 angular.module('attero')
-    .directive('tileElementGrid', ['$parse', function($parse) {
-        function buildOffsetArray(rowCount, columnCount) {
-            var rows = [];
-            var rowOffset = 0;
-
-            for (var i=0; i<rowCount; i++) {
-                console.log('row')
-                var row = [];
-                var columnOffset = 0;
-
-                for (var j=0; j<columnCount; j++) {
-                    console.log('column')
-                    row.push({ x: columnOffset, y: rowOffset });
-                    columnOffset += 40;
-                }
-
-                rows.push(row);
-                rowOffset += 40;
-            }
-
-            return rows;
-        }
-
+    .directive('tileElementPreview', ['$parse', function($parse) {
         return {
             scope: true,
-            templateUrl: '/assets/js/angular/views/includes/tileElementGrid.html',
-            replace: true,
+            templateUrl: '/assets/js/angular/views/includes/tileElementPreview.html',
+            // replace: true,
             link: function (scope, element, attrs) {
-                var tileElementParser = $parse(attrs.source);
+                var preview = $('.preview', element);
 
-                scope.tileElement = tileElementParser(scope);
-                scope.offsetArray = buildOffsetArray(
-                    scope.tileElement.getRowCount(),
-                    scope.tileElement.getColumnCount()
-                );
+                preview.css('width', 40);
+                preview.css('height', 40);
+                preview.css('background-image', 'url("' + scope.tileElement.img.src + '")');
+                preview.css('background-position', scope.column.x + 'px ' + scope.column.y + 'px');
 
-                console.log(scope.offsetArray)
+                // console.log(scope.tileElement)
+                // var tileElementParser = $parse(attrs.source);
+
+                // scope.tileElement = tileElementParser(scope);
+                // scope.offsetArray = buildOffsetArray(
+                //     scope.tileElement.getRowCount(),
+                //     scope.tileElement.getColumnCount()
+                // );
+
+                // console.log(scope.offsetArray)
 
                 // scope.rowCount = scope.tileElement.getRowCount();
                 // scope.columnCount = scope.tileElement.getColumnCount();
