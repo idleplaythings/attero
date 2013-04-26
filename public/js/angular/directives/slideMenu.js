bf32896a7b61bac455dcb6573d513eae9c34f99b
Niko Nevala <niko@nevala.fi>
2013-04-26 12:00:53 +0100

    fix slide menu positioning and styling

--- public/js/angular/directives/slideMenu.js
+++ public/js/angular/directives/slideMenu.js
@@ -26,25 +26,27 @@ angular.module('attero')
             },
             templateUrl: '/assets/js/angular/views/includes/slideMenu.html',
             replace: true,
+            controller: function($scope) {
+                $scope.select = function(item) {
+                    $scope.menuService.select(item);
+                }
+            },
             link: function (scope, element, attrs) {
                 scope.menuService = getMenuService(attrs.service);
+                scope.selected = null;
+                scope.dock = attrs.dock;
 
                 scope.rows = splitMenuItemsIntoRows(
                     scope.menuService.getItems(attrs.service),
                     attrs.columns
                 );
 
-                scope.select = function(item) {
-                    scope.menuService.select(item);
-                }
-
-                scope.selected = null;
-
                 scope.$watch('menuService.getSelected()', function(newValue, oldValue) {
                     scope.selected = newValue;
                 });
 
                 element.css('top', attrs.top + 'px');
+                element.css(attrs.dock, 20 - element.outerWidth());
 
                 element.on('mouseover', function() {
                     element.css(attrs.dock, '0px');
