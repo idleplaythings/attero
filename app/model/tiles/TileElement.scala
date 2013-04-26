3b30abf97ebb5c1895dc2b23e89002cf1b9785cf
Niko Nevala <niko@nevala.fi>
2013-04-26 12:00:26 +0100

    remove duplicate class definitions

--- app/model/tiles/TileElement.scala
+++ app/model/tiles/TileElement.scala
@@ -1,15 +1,5 @@
 package models.tiles
 
-class TileElement(
-    val elementType: Int,
-    val terrainDifficulty: Int,
-    val hide: Int,
-    val cover: Int,
-    val concealment: Int,
-    val height: Int)
-{
-}
-
 trait Continious
 {
 }
--- app/model/tiles/TileTexture.scala
+++ app/model/tiles/TileTexture.scala
@@ -1,10 +1,2 @@
 package models.tiles
 
-class TileTexture(
-    val texturetype: Int,
-    val terrainDifficulty: Int,
-    val hide: Int,
-    val cover: Int,
-    val concealment: Int)
-{
-}
